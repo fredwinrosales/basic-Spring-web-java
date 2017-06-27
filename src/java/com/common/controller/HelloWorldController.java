d2a66205b460d5ca2d5e235b710dbcf468b8ec6f
Fredwin Rosales <fredwinrosales@gmail.com>
2017-06-27 07:19:56 -0400

    Bucle for
    Se aplica bucle for desde vista

--- src/java/com/common/controller/HelloWorldController.java
+++ src/java/com/common/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.common.controller;
 
+import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,8 +8,13 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloWorldController{
 
+    private ArrayList<String> saludo;
+    
 	@RequestMapping("/welcome")
 	public ModelAndView helloWorld() {
-            return new ModelAndView("HelloWorldPage");
+            saludo = new ArrayList<>();
+            saludo.add("Hola mundo 1");
+            saludo.add("Hola mundo 2");
+            return new ModelAndView("HelloWorldPage", "saludo", saludo);
         }
 }
--- web/WEB-INF/pages/HelloWorldPage.jsp
+++ web/WEB-INF/pages/HelloWorldPage.jsp
@@ -1,6 +1,13 @@
+<%@page import="java.util.ArrayList"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <html>
 <body>
 	<h1>Spring MVC Hello World Example</h1>
+        
+        <p>Este es un saludo: ${saludo[0]}</p>
+        
+        <c:forEach var="listValue" items="${saludo}">
+                <li>${listValue}</li>
+        </c:forEach>
 </body>
 </html>
